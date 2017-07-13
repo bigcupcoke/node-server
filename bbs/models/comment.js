0a87773194ad97fcf4b7df9026983fff690cfaf3
coke.dj <dujun.sc@outlook.com>
2017-07-13 23:06:03 +0800

    weibo 评论功能的model

---
+++ bbs/models/comment.js
@@ -0,0 +1,28 @@
+const Model = require('./main')
+const User = require('./user')
+const Weibo = require('./weibo')
+
+class Comment extends Model {
+    constructor(form={}, user_id=-1) {
+        super()
+        this.id = form.id
+        this.content = form.content || ''
+        // 和别的数据关联的方式, 用 user_id 表明拥有它的 user 实例
+        this.user_id = Number(form.user_id || user_id)
+        this.weibo_id = Number(form.weibo_id || -1)
+    }
+
+    user() {
+        const u = User.findOne('id', this.user_id)
+        return u
+    }
+
+    weibo() {
+        // id 是微博的属性
+        // weibo_id 是评论的属性
+        const w = Weibo.findOne('id', this.weibo_id)
+        return w
+    }
+}
+
+module.exports = Comment
