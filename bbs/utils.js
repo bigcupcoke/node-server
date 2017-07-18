b4276f728d7620c827b24ad916d9a8372bb2d070
coke.dj <dujun.sc@outlook.com>
2017-07-18 17:31:43 +0800

    error 放到 utils.js中

--- bbs/utils.js
+++ bbs/utils.js
@@ -40,9 +40,21 @@ const randomStr = () => {
     return s
 }
 
+const error = (request=null, code=404) => {
+    const e = {
+        404: 'HTTP/1.1 404 NOT FOUND\r\n\r\n<h1>NOT FOUND</h1>',
+    }
+    const r = e[code] || ''
+    return r
+}
+
+const key = 'dake123'
+
 module.exports = {
     log,
     randomStr,
+    error,
+    key,
 }
 
 
