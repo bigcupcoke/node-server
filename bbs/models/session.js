6b46f80bfa708ffacc8de18b7d0df74428424eef
coke.dj <dujun.sc@outlook.com>
2017-07-18 14:43:45 +0800

    session Model c层

---
+++ bbs/models/session.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto')
+
+const Model = require('./main')
+
+const { key } = require('../utils')
+
+class Session extends Model {
+    constructor(form={}) {
+        super()
+        this.key = key
+        this.algorithm = 'aes-256-cbc'
+        this.input = 'utf8'
+        this.output = 'hex'
+        this.content = form
+    }
+
+    decyrpt(c) {
+        var decipher = crypto.createDecipher(this.algorithm, key)
+        var d = decipher.update(c, this.output, this.input)
+        d += decipher.final(this.input)
+        const r = JSON.parse(d)
+        return r
+    }
+
+    encrypt(form) {
+        const s = JSON.stringify(form)
+        const cipher = crypto.createCipher(this.algorithm, key)
+        let c = cipher.update(s, this.input, this.output)
+        c += cipher.final(this.output)
+        return c
+    }
+}
+
+const session = new Session()
+module.exports = session
